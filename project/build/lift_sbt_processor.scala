931e2d1fd1a1bc7dd19f1f5fef8a8a242d66bc04
Mads Hartmann <mads379@gmail.com>
2010-06-25 10:52:24 +0200

    Removed .DS_Store files and tried to figure out why SBT isn't fetching the depedency declared with an explicit url in SBT template engine

--- project/build/lift_sbt_processor.scala
+++ project/build/lift_sbt_processor.scala
@@ -4,12 +4,12 @@ class LiftSBTProcessor(info: ProjectInfo) extends ProcessorProject(info) {
 	
 	// repos
 	val nexus = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
+	
 	// dependencies
-	val scalate = "org.fusesource.scalate" % "scalate-core" % "1.0-local" from "http://github.com/downloads/mads379/Simple-Build-Tool-Template-Engine/scalate-core-1.0-SNAPSHOT.jar"
 	val sbt_template_engine = "com.sidewayscoding" % "sbt_template_engine_2.7.7" % "0.1"
 	
 	// PUBLISHING
-	override def managedStyle = ManagedStyle.Maven
+	override def managedStyle = ManagedStyle.Ivy
 	val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
 	Credentials(Path.userHome / "dev" / ".nexus_credentials", log)
 	

0c5cf2b5e1b6330119c3104299cbc48761dfc384
Mads Hartmann <mads379@gmail.com>
2010-07-21 11:53:44 +0200

    Bumping to SNAPSHOT version

--- project/build/lift_sbt_processor.scala
+++ project/build/lift_sbt_processor.scala
@@ -5,15 +5,15 @@ class LiftSBTProcessor(info: ProjectInfo) extends ProcessorProject(info) {
 	val scalatools_release = "Scala Tools Releases" at "http://scala-tools.org/repo-releases/"
 	val scalatools_snapshots = "Scala Tools Snapshot" at "http://scala-tools.org/repo-snapshots/"
 	
-  // val sbt_template_engine = "com.sidewayscoding" % "sbt_template_engine_2.7.7" % "0.1-SNAPSHOT"
-  val sbt_template_engine = "com.sidewayscoding" % "sbt_template_engine_2.7.7" % "0.2"
+  val sbt_template_engine = "com.sidewayscoding" % "sbt_template_engine_2.7.7" % "0.2-SNAPSHOT"
+  // val sbt_template_engine = "com.sidewayscoding" % "sbt_template_engine_2.7.7" % "0.2"
   val scalatest = "org.scalatest" % "scalatest" % "1.0" % "test"
   
   
 	
 	override def managedStyle = ManagedStyle.Maven
-  val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
-  // val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/snapshots/"
+  // val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
+  val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/snapshots/"
 	Credentials(Path.userHome / "dev" / ".nexus_credentials", log)
 	
 }

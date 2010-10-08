53a6c3d21a686f15858c527649706e0d0b95f910
Mads Hartmann <mads379@gmail.com>
2010-10-08 21:36:29 +0100

    Master version is version 0.3 with a dependency of lifty-engine 0.4

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -5,15 +5,11 @@ class Project(info: ProjectInfo) extends ProcessorProject(info) with ProguardPro
 	val scalatools_release = "Scala Tools Releases" at "http://scala-tools.org/repo-releases/"
 	val scalatools_snapshots = "Scala Tools Snapshot" at "http://scala-tools.org/repo-snapshots/"
 	
-  val sbt_template_engine = "org.lifty" %% "lifty-engine" % "0.4-SNAPSHOT"
-  // val sbt_template_engine = "org.lifty" %% "lifty-engine" % "0.4-LOCAL"
-  // val sbt_template_engine = "org.lifty" %% "lifty-engine" % "0.4"
+  val sbt_template_engine = "org.lifty" %% "lifty-engine" % "0.4"
   val scalatest = "org.scalatest" % "scalatest" % "1.0" % "test"
-  
-	
+  	
 	override def managedStyle = ManagedStyle.Maven
-  //val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
-  val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/snapshots/"
+  val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
 	Credentials(Path.userHome / "dev" / ".nexus_credentials", log)
 	
 	//proguard plugin customizations

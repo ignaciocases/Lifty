e63998858abb7c062dfec14bc04b39ac85ae6257
Mads Hartmann <mads379@gmail.com>
2010-10-27 02:21:52 +0200

    Just published 1.4 so next version will be 1.5

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -5,11 +5,11 @@ class Project(info: ProjectInfo) extends ProcessorProject(info) with AssemblyPro
   val scalatools_release = "Scala Tools Releases" at "http://scala-tools.org/repo-releases/"
   val scalatools_snapshots = "Scala Tools Snapshot" at "http://scala-tools.org/repo-snapshots/"
 
-  val sbt_template_engine = "org.lifty" %% "lifty-engine" % "0.4-SNAPSHOT"
+  val sbt_template_engine = "org.lifty" %% "lifty-engine" % "0.6"
   val scalatest = "org.scalatest" % "scalatest" % "1.0" % "test"
 
   override def managedStyle = ManagedStyle.Maven
-  val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/snapshots/"
+  val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
   Credentials(Path.userHome / "dev" / ".nexus_credentials", log)
 
   override def mainClass: Option[String] = Some("org.lifty.processor.LiftyStandAlone")
